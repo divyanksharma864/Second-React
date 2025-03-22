dd58361d61554a1ae9ff2957e6d57d12b95b4264
divyanksharma864 <divyanksharma22@gmail.com>
2025-03-22 01:23:21 +0100

    Travel Journal header design by CSS done

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+        <header>
+            <img className="globe" src="globe.png" alt="globe" width="50%" />
+            <h1> My travel journal.</h1>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@
 // )
 
 import {createRoot} from "react-dom/client"
-import {App} from "./App"
+import App from "./App"
 /**
  * Challenge:
  * 
